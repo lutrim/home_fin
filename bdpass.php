d991275691d178c387725a52f7bb15231f2d63e3
lutrin <lutrin.shop@gmail.com>
2013-05-15 23:59:53 -0700

    modify bdpass

--- bdpass.php
+++ bdpass.php
@@ -1,9 +1,8 @@
 <?php
 
 global $bdhost, $bdname, $bduser, $bdpass;
-$bdhost = 'mysql.lutrim.com'; 
-$bdname = 'lut_fin_test'; 
-$bduser = 'fin_root'; 
-$bdpass = 'fin_prog';
-
-?>
+$bdhost = 'mysql HOST'; 
+$bdname = 'some name'; 
+$bduser = 'some user; 
+$bdpass = '8 char/number simbols';
+>
