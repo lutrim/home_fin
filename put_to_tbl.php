24f88f6e115026125337bd7e9faea9194ce842cd
lutrin <lutrin.shop@gmail.com>
2014-05-17 13:41:32 -0700

    minor fix double post summ

--- put_to_tbl.php
+++ put_to_tbl.php
@@ -19,7 +19,7 @@ if ($_POST["credit_check"] === "on" and $_POST["op_group"] === "2") {
 
 add_operation($_POST);
 //проверяем, если это приход средств для погашения кредита, то проводим еще одну операцию для снятия денег с основного остатка
-if ($_POST["credit_check"] === "on") {
+if ($_POST["credit_check"] === "on" and $_POST["op_group"] === "2") {
 	$_POST["op_summ"] = -1 * abs($_POST["op_summ"]);
 	$_POST["op_group"] = 0;
 	$_POST["credit_check"] = "off";
